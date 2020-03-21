7864e073c4b70d43ee45b42b1e4501087ec4663f
omgimanerd <alvin@omgimanerd.tech>
2020-03-21 15:55:03 -0400

    Fix eslint errors and update .gitignore

--- server/errors.js
+++ server/errors.js
@@ -3,7 +3,17 @@
  * @author alvin@omgimanerd.tech (Alvin Lin)
  */
 
+/**
+ * Encapsulates a class of error that we can gracefully handle and should
+ * log for debugging.
+ * @extends Error
+ */
 class RecoverableError extends Error {
+  /**
+   * Constructor for a Recoverable Error which stores useful information
+   * in the internal message field.
+   * @param {*} e Error or string representable object
+   */
   constructor(e) {
     super()
     if (e instanceof Error) {
--- server/loggers.js
+++ server/loggers.js
@@ -8,7 +8,6 @@ const winston = require('winston')
 const util = require('util')
 
 module.exports = options => {
-  const analyticsFile = options.analyticsFile
   const errorFile = options.errorFile
 
   const errorLogger = new winston.Logger({
@@ -31,8 +30,8 @@ module.exports = options => {
       ],
       expressFormat: true,
       colorize: true,
-      dynamicMeta: (request, response) => {
-        ip: request.headers['x-forwarded-for'] || request.ip
+      dynamicMeta: request => {
+        request.ip = request.headers['x-forwarded-for'] || request.ip
       }
     }),
     logError: data => {
